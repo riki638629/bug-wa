0c07efd402abd7e612f2df05441660cc28761d1e
riki638629 <ppj62678@gmail.com>
2025-11-10 21:42:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8251924798:AAHQgxIDsR0CNFqEzLxO-qe3pgbm74nQ2S4",  // Ubah Jadi Token Bot Mu !!!
+  owner: "8257490486", // Ubah Jadi Id Mu !!!
+  port: "1466", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "167.172.64.240" // Ubah Jadi Ip Vps Mu !!!
+};
